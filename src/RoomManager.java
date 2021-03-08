97adb238d485431256d977a417e4371c5169e2f0
Owen <owemarco@gmail.com>
2021-03-08 13:35:00 -0800

    Creating system for saving and loading dungeon layout.

--- src/RoomManager.java
+++ src/RoomManager.java
@@ -1,100 +1,128 @@
-
+import java.awt.Rectangle;
 
 public class RoomManager {
 //Manages rooms, sets them up.
 	
-	Room[][][] dungeon1 = {
-			{ //Floor 7
-				{/* , , , , , , ,*/},
-				{/* , , , , , , ,*/},
-				{/* , , , , , , ,*/},
-				{/* , , , , , , ,*/},
-				{/* , , , , , , ,*/},
-				{/* , , , , , , ,*/},
-				{/* , , , , , , ,*/}
-			},
-			{ //FLoor 6
-				{/* , , , , , , ,*/},
-				{/* , , , , , , ,*/},
-				{/* , , , , , , ,*/},
-				{/* , , , , , , ,*/},
-				{/* , , , , , , ,*/},
-				{/* , , , , , , ,*/},
-				{/* , , , , , , ,*/}
-			},
-			{ //FLoor 5
-				{/* , , , , , , ,*/},
-				{/* , , , , , , ,*/},
-				{/* , , , , , , ,*/},
-				{/* , , , , , , ,*/},
-				{/* , , , , , , ,*/},
-				{/* , , , , , , ,*/},
-				{/* , , , , , , ,*/}
-			},
-			{ //FLoor 4
-				{/* , , , , , , ,*/},
-				{/* , , , , , , ,*/},
-				{/* , , , , , , ,*/},
-				{/* , , , , , , ,*/},
-				{/* , , , , , , ,*/},
-				{/* , , , , , , ,*/},
-				{/* , , , , , , ,*/}
-			},
-			{ //FLoor 3
-				{/* , , , , , , ,*/},
-				{/* , , , , , , ,*/},
-				{/* , , , , , , ,*/},
-				{/* , , , , , , ,*/},
-				{/* , , , , , , ,*/},
-				{/* , , , , , , ,*/},
-				{/* , , , , , , ,*/}
-			},
-			{ //FLoor 2
-				{/* , , , , , , ,*/},
-				{/* , , , , , , ,*/},
-				{/* , , , , , , ,*/},
-				{/* , , , , , , ,*/},
-				{/* , , , , , , ,*/},
-				{/* , , , , , , ,*/},
-				{/* , , , , , , ,*/}
-			},
-			{ //FLoor 1
-				{/* , , , , , , ,*/},
-				{/* , , , , , , ,*/},
-				{/* , , , , , , ,*/},
-				{/* , , , , , , ,*/},
-				{/* , , , , , , ,*/},
-				{/* , , , , , , ,*/},
-				{/* , , , , , , ,*/}
-			},
-	};
+	Dungeon dungeon1;
+	
+	Dungeon loadedDungeon;
 	
 	
-	Room[][][] loadedDungeon = dungeon1;
+	
+	
 	
 	RoomManager() {
+		dungeon1 = new Dungeon(new Room[/*Floor*/][/*Row*/][/*Column*/] {
+			{ //FLoor 2
+				{    null   ,    null   ,    null   ,    null    },
+				{    null   , new Room(), new Room(), new Room() },
+				{    null   ,    null   ,    null   ,    null    }
+			},
+			{ //FLoor 1
+				{    null   , new Room(),    null   ,    null    },
+				{ new Room(), new Room(), new Room(), new Room() },
+				{    null   , new Room(),    null   ,    null    }
+			},
+		});
+		loadedDungeon = dungeon1;
+		dungeon1.dungeonMap[1][3][2].roomMap = new roomObject[][] {
+			{/* 16 floors/walls/traps/etc.*/},
+			{},
+			{},
+			{},
+			{},
+			{},
+			{},
+			{},
+			{},
+			{},
+			{},
+			{},
+			{},
+			{},
+			{},
+			{}
+		};
 		
 	}
 	
 	
 }
 
-
+class Dungeon {
+	Room startingRoom;
+	Room[][][] dungeonMap;
+	
+	Dungeon(Room[][][] dungeonMap) {
+		this.dungeonMap = dungeonMap;
+	}
+	
+}
 
 class Room {
-	//Each room is an 80 by 80 grid off 10 pxl by 10 pxl squares. Each square can hold one object, such as a wall, a floor, a pit, etc.
-	Object[][] room = new Object[ZeldaDungeon.WIDTH/10][ZeldaDungeon.HEIGHT/10];
+	//Each room is a 16 by 16 grid off 50 pxl by 50 pxl squares. Each square can hold one object, such as a wall, a floor, a pit, etc.
+	roomObject[][] roomMap;
+	
 	
 }
 
-class Wall {
-	
-}
 
-class Trap {
-	public static final int PIT_TRAP = 1;
+/*–––––––––––––––––––––––––––––––––––––––––––Room Object Stuff––––––––––––––––––––––––––––––––––––––––––––––––*/
+
+
+class roomObject {
+	//Base class of each item in a room's grid
 	int type;
+	int cornerX;
+	int cornerY;
+	Rectangle collisionBox;
+	
+	roomObject(int type) {
+		this.type = type;
+	}
+	
+	void draw() {
+		
+	}
 }
-class Floor {
+
+
+
+class Wall extends roomObject {
+	public static final int NORMAL = 1;
+	public static final int DOOR = 2;
+	public static final int TORCH = 3;
+	
+	Wall(int type) {
+		super(type);
+		// TODO Auto-generated constructor stub
+	}
+
+	
+}
+
+class Trap extends roomObject {
+	public static final int PIT_TRAP = 1;
+	
+	Trap(int type) {
+		super(type);
+		// TODO Auto-generated constructor stub
+	}
+
+	
+	
+}
+class Floor extends roomObject {
+	Object spawningObject;
+	Floor(int type) {
+		super(type);
+		// TODO Auto-generated constructor stub
+	}
+	//If the floor spawns with an object on it (like an enemy), it will use this constructor
+	Floor(int type, Object spawningObject) {
+		super(type);
+		this.spawningObject = spawningObject;
+		// TODO Auto-generated constructor stub
+	}
 	
 }
